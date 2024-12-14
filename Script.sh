2a686befcc50b196e606a13db447afde51b78cf0
Vpretto0 <victoriapretto035@gmail.com>
2024-12-14 15:57:47 -0600

    mas ajustes

--- Script.sh
+++ Script.sh
@@ -2746,10 +2746,8 @@ echo "Configuring additional password policies in pwquality.conf..."
 # 5.3.3.2.8(new) Enforce password quality is enforced for the root user
 #NO SÉ SI ESTÁ REPETIDO
 {
- [ ! -d /etc/security/pwquality.conf.d/ ] && mkdir
-/etc/security/pwquality.conf.d/
- printf '\n%s\n' "enforce_for_root" > /etc/security/pwquality.conf.d/50-
-pwroot.conf
+ [ ! -d /etc/security/pwquality.conf.d/ ] && mkdir /etc/security/pwquality.conf.d/
+ printf '\n%s\n' "enforce_for_root" > /etc/security/pwquality.conf.d/50- pwroot.conf
 }
 
 #INTENTO DE 5.3.2.4
